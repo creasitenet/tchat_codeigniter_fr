0da6462fdce090fcc56338c3705a0668f2d3d0d5
Edouard Boissel <creasitenet.com@gmail.com>
2015-10-03 18:56:19 +0200

    Update _users.php

--- application/views/tchat/_users.php
+++ application/views/tchat/_users.php
@@ -2,9 +2,11 @@
     <!-- Users -->
     <?php if (isset($users)): ?> 
     	<?php if(!empty($users)): ?>
+		<ul class="users">
 			<?php foreach ($users as $u): ?>
 				<li class="username"><?php echo $u->username; ?></li>
 			<?php endforeach; ?>
+		</ul>
 		<?php endif; ?>
     <?php endif; ?>
     <!-- //Users -->
